a2e228c541c3fdd5d7c13a8fa92bc4ad93e98261
dhcrain <dhcrain@gmail.com>
2016-06-28 20:10:53 -0400

    added client in main.py

--- main.py
+++ main.py
@@ -7,31 +7,45 @@ def print_results(response, dict_key, number):
 
 def call_api(data):
     number = 1
-    url, dict_key = data
+    url, dict_key, url_key = data
     response = requests.get(url).json()
     print_results(response, dict_key, number)
     number += 100
     if response['next']:
         while response['next']:
-            more = input("Do you want to see more? Y/n \n").lower()
-            if more != "n":
+            more = input("* Do you want to see more? Y/n \n* or # of item to see details.\n").lower()
+            if more == "y":
                 url = response['next']
                 response = requests.get(url).json()
                 print_results(response, dict_key, number)
                 number += 100
+            elif more == "n":
+                welcome()
             else:
-                exit()
+                more = int(more)
+                url = 'http://127.0.0.1:8000/api/{}/{}/'.format(url_key, more)
+                response = requests.get(url).json()
+                for key, value in response.items():
+                    print('{}: {}'.format(key, value))
+                welcome()
 
-choice = int(input('''
+def welcome():
+    choice = int(input('''
 What do you want to see?
 1: Movies
 2: Raters
 3: Rating
-'''))
+4: Exit
+    '''))
 
-choice_dict = {
-1: ["http://127.0.0.1:8000/api/movies/", "movie_title"],
-2: ["http://127.0.0.1:8000/api/raters/", "age"],
-3: ["http://127.0.0.1:8000/api/ratings/", "rating"],
-}
-call_api(choice_dict[choice])
+    if choice == 4:
+        exit()
+    else:
+        choice_dict = {
+        1: ["http://127.0.0.1:8000/api/movies/", "movie_title", "movies"],
+        2: ["http://127.0.0.1:8000/api/raters/", "age", "raters"],
+        3: ["http://127.0.0.1:8000/api/ratings/", "rating", "ratings"],
+        }
+        call_api(choice_dict[choice])
+
+welcome()
